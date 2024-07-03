f6d6c864640e74da2ee414365ffaed0ad60d21f1
Jacob Söderblom <253415@skane.se>
2024-07-03 13:30:20 +0200

    kickstarter

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,15 +1,23 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
     lazypath,
-  })
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("vim-options")
-require("lazy").setup("plugins")
+require 'vim-options'
+require('lazy').setup {
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+
+  { import = 'plugins' },
+}
--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,13 +1,16 @@
-return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-	end,
+-- lua/plugins/neo-tree.lua
+local kickstart_neo_tree = require 'kickstart.plugins.neo-tree'
+
+-- Extend the configuration
+kickstart_neo_tree.keys = {
+  { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
+  { '<C-n>', ':Neotree filesystem reveal left<CR>', { noremap = true, silent = true, desc = 'NeoTree reveal left' } },
+  { '<leader>bf', ':Neotree buffers reveal float<CR>', { noremap = true, silent = true, desc = 'NeoTree buffers float' } },
 }
+
+kickstart_neo_tree.config = function()
+  -- Default configuration from kickstart
+  require('neo-tree').setup(kickstart_neo_tree.opts)
+end
+
+return kickstart_neo_tree
