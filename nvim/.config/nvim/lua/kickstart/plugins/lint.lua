5bc417681ac744bcfebcb0323d3fb8e73fda04cc
Jacob Söderblom <253415@skane.se>
2025-03-05 16:49:27 +0100

    Updating nvim config
    Aligning it with kickstarter.nvim and also adding some new plugins

--- nvim/.config/nvim/lua/kickstart/plugins/lint.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/lint.lua
@@ -1,55 +1,61 @@
+-- Adds git related signs to the gutter, as well as utilities for managing changes
+-- NOTE: gitsigns is already included in init.lua but contains only the base
+-- config. This will add also the recommended keymaps.
+
 return {
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      on_attach = function(bufnr)
+        local gitsigns = require 'gitsigns'
 
-  { -- Linting
-    'mfussenegger/nvim-lint',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      local lint = require 'lint'
-      lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
-      }
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
 
-      -- To allow other plugins to add linters to require('lint').linters_by_ft,
-      -- instead set linters_by_ft like this:
-      -- lint.linters_by_ft = lint.linters_by_ft or {}
-      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
-      --
-      -- However, note that this will enable a set of default linters,
-      -- which will cause errors unless these tools are available:
-      -- {
-      --   clojure = { "clj-kondo" },
-      --   dockerfile = { "hadolint" },
-      --   inko = { "inko" },
-      --   janet = { "janet" },
-      --   json = { "jsonlint" },
-      --   markdown = { "vale" },
-      --   rst = { "vale" },
-      --   ruby = { "ruby" },
-      --   terraform = { "tflint" },
-      --   text = { "vale" }
-      -- }
-      --
-      -- You can disable the default linters by setting their filetypes to nil:
-      -- lint.linters_by_ft['clojure'] = nil
-      -- lint.linters_by_ft['dockerfile'] = nil
-      -- lint.linters_by_ft['inko'] = nil
-      -- lint.linters_by_ft['janet'] = nil
-      -- lint.linters_by_ft['json'] = nil
-      -- lint.linters_by_ft['markdown'] = nil
-      -- lint.linters_by_ft['rst'] = nil
-      -- lint.linters_by_ft['ruby'] = nil
-      -- lint.linters_by_ft['terraform'] = nil
-      -- lint.linters_by_ft['text'] = nil
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { ']c', bang = true }
+          else
+            gitsigns.nav_hunk 'next'
+          end
+        end, { desc = 'Jump to next git [c]hange' })
 
-      -- Create autocommand which carries out the actual linting
-      -- on the specified events.
-      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-        group = lint_augroup,
-        callback = function()
-          lint.try_lint()
-        end,
-      })
-    end,
+        map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { '[c', bang = true }
+          else
+            gitsigns.nav_hunk 'prev'
+          end
+        end, { desc = 'Jump to previous git [c]hange' })
+
+        -- Actions
+        -- visual mode
+        map('v', '<leader>hs', function()
+          gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = 'stage git hunk' })
+        map('v', '<leader>hr', function()
+          gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = 'reset git hunk' })
+        -- normal mode
+        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
+        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
+        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
+        map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = 'git [u]ndo stage hunk' })
+        map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
+        map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
+        map('n', '<leader>hb', gitsigns.blame_line, { desc = 'git [b]lame line' })
+        map('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
+        map('n', '<leader>hD', function()
+          gitsigns.diffthis '@'
+        end, { desc = 'git [D]iff against last commit' })
+        -- Toggles
+        map('n', '<leader>gb', gitsigns.toggle_current_line_blame, { desc = 'Toggle [g]it show [b]lame line' })
+        map('n', '<leader>gD', gitsigns.toggle_deleted, { desc = 'Toggle [g]it show [D]eleted' })
+      end,
+    },
   },
 }
--- nvim/.config/nvim/lua/plugins/csharp.lua
+++ nvim/.config/nvim/lua/plugins/csharp.lua
@@ -1,97 +1,97 @@
+local function add_dotnet_mappings()
+  local dotnet = require 'easy-dotnet'
+
+  vim.api.nvim_create_user_command('Secrets', function()
+    dotnet.secrets()
+  end, {})
+
+  vim.keymap.set('n', '<C-t>', function()
+    vim.cmd 'Dotnet testrunner'
+  end, { nowait = true })
+
+  vim.keymap.set('n', '<C-p>', function()
+    dotnet.run_with_profile(true)
+  end, { nowait = true })
+
+  vim.keymap.set('n', '<C-b>', function()
+    dotnet.build_default_quickfix()
+  end, { nowait = true })
+end
+
 -- lazy.nvim
 return {
   {
     'seblj/roslyn.nvim',
     ft = 'cs',
-    opts = {
-      -- your configuration comes here; leave empty for default settings
-    },
+    config = function()
+      require('roslyn').setup {
+        config = {
+          settings = {
+            ['csharp|background_analysis'] = {
+              dotnet_compiler_diagnostics_scope = 'fullSolution',
+            },
+            ['csharp|inlay_hints'] = {
+              csharp_enable_inlay_hints_for_implicit_object_creation = true,
+              csharp_enable_inlay_hints_for_implicit_variable_types = true,
+              csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+              csharp_enable_inlay_hints_for_types = true,
+              dotnet_enable_inlay_hints_for_indexer_parameters = true,
+              dotnet_enable_inlay_hints_for_literal_parameters = true,
+              dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+              dotnet_enable_inlay_hints_for_other_parameters = true,
+              dotnet_enable_inlay_hints_for_parameters = true,
+              dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+              dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+              dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+            },
+            ['csharp|code_lens'] = {
+              dotnet_enable_references_code_lens = true,
+            },
+          },
+        },
+        filewatching = not vim.g.is_perf,
+      }
+    end,
   },
   {
     'GustavEikaas/easy-dotnet.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim', 'stevearc/overseer.nvim', 'akinsho/toggleterm.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
     config = function()
-      require('overseer').setup()
-      local logPath = vim.fn.stdpath 'data' .. '/easy-dotnet/build.log'
       local dotnet = require 'easy-dotnet'
-
       dotnet.setup {
-        terminal = function(path, action)
-          local commands = {
-            run = function()
-              return 'dotnet run --project ' .. path
-            end,
-            test = function()
-              return 'dotnet test ' .. path
-            end,
-            restore = function()
-              return 'dotnet restore ' .. path
-            end,
-            build = function()
-              return 'dotnet build  ' .. path .. ' /flp:v=q /flp:logfile=' .. logPath
-            end,
-          }
-
-          local function filter_warnings(line)
-            if not line:find 'warning' then
-              return line:match '^(.+)%((%d+),(%d+)%)%: (.+)$'
-            end
-          end
-
-          local overseer_components = {
-            { 'on_complete_dispose', timeout = 30 },
-            'default',
-            { 'unique', replace = true },
-            {
-              'on_output_parse',
-              parser = {
-                diagnostics = {
-                  { 'extract', filter_warnings, 'filename', 'lnum', 'col', 'text' },
-                },
-              },
-            },
-            {
-              'on_result_diagnostics_quickfix',
-              open = true,
-              close = true,
-            },
-          }
-
-          local function get_git_root()
-            local git_root = vim.fn.systemlist('git rev-parse --show-toplevel')[1]
-            if vim.v.shell_error ~= 0 then
-              return nil -- Not in a Git repo
-            end
-            return git_root
-          end
-
-          if action == 'run' or action == 'test' then
-            table.insert(overseer_components, { 'restart_on_save', paths = { get_git_root() } })
-          end
-
-          local command = commands[action]()
-          local task = require('overseer').new_task {
-            strategy = {
-              'toggleterm',
-              use_shell = false,
-              direction = 'float',
-              open_on_start = true,
-            },
-            name = action,
-            cmd = command,
-            cwd = get_git_root(),
-            components = overseer_components,
-          }
-          task:start()
-        end,
-
-        testrunner = {
+        test_runner = {
+          enable_buffer_test_execution = true,
           viewmode = 'float',
         },
+        auto_bootstrap_namespace = true,
+        terminal = function(path, action, args)
+          local commands = {
+            run = function()
+              return string.format('dotnet run --project %s %s', path, args)
+            end,
+            test = function()
+              return string.format('dotnet test %s %s', path, args)
+            end,
+            restore = function()
+              return string.format('dotnet restore %s %s', path, args)
+            end,
+            build = function()
+              return string.format('dotnet build %s %s', path, args)
+            end,
+          }
+
+          local command = commands[action]() .. '\r'
+          require('toggleterm').exec(command, nil, nil, nil, 'float')
+        end,
       }
 
-      vim.keymap.set('n', '<leader>ct', dotnet.testrunner, { desc = 'Testrunner' })
-      vim.keymap.set('n', '<leader>cr', dotnet.testrunner, { desc = 'Testrunner refresh' })
+      vim.api.nvim_create_autocmd('VimEnter', {
+        callback = function()
+          if dotnet.is_dotnet_project() then
+            add_dotnet_mappings()
+          end
+        end,
+      })
     end,
   },
 }
