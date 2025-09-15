3693c629b26593eadfc5de1fa211c94a2b687f92
Jacob Söderblom <jacob.soderblom@protonmail.com>
2025-09-15 09:49:37 +0200

    feat: switch to github dark high contrast

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,35 +1,16 @@
 return {
   {
-    'catppuccin/nvim',
+    'projekt0n/github-nvim-theme',
     priority = 1000,
 
     opts = {
-      transparent_background = true,
-      dim_inactive = {
-        enabled = false,
-        shade = 'dark',
-        percentage = 0.15,
-      },
-      integrations = {
-        cmp = true,
-        dashboard = true,
-        gitsigns = true,
-        leap = true,
-        mason = true,
-        notify = true,
-        treesitter = true,
-        noice = true,
-        nvim_surround = true,
-        snacks = {
-          enabled = true,
-        },
-        which_key = false,
-      },
+      theme_style = 'dark_high_contrast',
+      transparent = true,
     },
 
     config = function(_, opts)
-      require('catppuccin').setup(opts)
-      vim.cmd.colorscheme 'catppuccin-mocha'
+      require('github-theme').setup(opts)
+      vim.cmd.colorscheme 'github_dark_high_contrast'
     end,
   },
 }
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -27,47 +27,47 @@ return {
 	-- Disable audio bell
 	audible_bell = "Disabled",
 
-	-- Colorscheme
-	colors = {
-		foreground = "#e0e0e0",
-		background = "#0f0f0f",
-		cursor_bg = "#e0e0e0",
-		cursor_border = "#e0e0e0",
-		cursor_fg = "#0f0f0f",
-		selection_fg = nil,
-		selection_bg = nil,
+        -- Colorscheme: GitHub Dark High Contrast
+        colors = {
+                foreground = "#f0f6fc",
+                background = "#0a0c10",
+                cursor_bg = "#f0f6fc",
+                cursor_border = "#f0f6fc",
+                cursor_fg = "#0a0c10",
+                selection_fg = "#0a0c10",
+                selection_bg = "#79c0ff",
 
-		-- Normal colors
-		ansi = {
-			"#151720", -- black
-			"#dd6777", -- red
-			"#90ceaa", -- green
-			"#ecd3a0", -- yellow
-			"#86aaec", -- blue
-			"#c296eb", -- magenta
-			"#93cee9", -- cyan
-			"#cbced3", -- white
-		},
+                -- Normal colors
+                ansi = {
+                        "#0a0c10", -- black
+                        "#ff7b72", -- red
+                        "#3fb950", -- green
+                        "#d29922", -- yellow
+                        "#58a6ff", -- blue
+                        "#bc8cff", -- magenta
+                        "#39c5cf", -- cyan
+                        "#c9d1d9", -- white
+                },
 
-		-- Bright colors
-		brights = {
-			"#4f5572", -- bright black (gray)
-			"#e26c7c", -- bright red
-			"#95d3af", -- bright green
-			"#f1d8a5", -- bright yellow
-			"#8baff1", -- bright blue
-			"#c79bf0", -- bright magenta
-			"#98d3ee", -- bright cyan
-			"#d0d3d8", -- bright white
-		},
+                -- Bright colors
+                brights = {
+                        "#484f58", -- bright black
+                        "#ffa198", -- bright red
+                        "#56d364", -- bright green
+                        "#e3b341", -- bright yellow
+                        "#79c0ff", -- bright blue
+                        "#d2a8ff", -- bright magenta
+                        "#56d4dd", -- bright cyan
+                        "#f0f6fc", -- bright white
+                },
 
-		-- URL highlight color
-		hyperlink_rules = {
-			{
-				regex = [[\b\w+://[\w.-]+\S*]],
-				format = "$0",
-				color = "#5de4c7",
-			},
-		},
-	},
+                -- URL highlight color
+                hyperlink_rules = {
+                        {
+                                regex = [[\b\w+://[\w.-]+\S*]],
+                                format = "$0",
+                                color = "#79c0ff",
+                        },
+                },
+        },
 }
